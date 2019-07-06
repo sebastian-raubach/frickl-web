7d2cc8fac1b3a6fea0a520eaa929a40c500edc96
Sebastian Raubach <sebastian.raubach@hutton.ac.uk>
2019-07-06 16:12:34 +0100

    - CHG: Changed default image node layout.

--- src/store.js
+++ src/store.js
@@ -11,7 +11,7 @@ export default new Vuex.Store({
     image: null,
     imageWidth: 'large',
     imagesPerPage: '12',
-    imageDetailsMode: 'overlay'
+    imageDetailsMode: 'below'
   },
   getters: {
     baseUrl: state => state.baseUrl,
