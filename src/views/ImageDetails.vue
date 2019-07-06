619d191a4af71c0626f878b1e0b3afeecc183048
Sebastian Raubach <sebastian.raubach@hutton.ac.uk>
2019-07-06 15:34:22 +0100

    - ADD: Added background transition to image details page.

--- src/views/ImageDetails.vue
+++ src/views/ImageDetails.vue
@@ -1,99 +1,101 @@
 <template>
   <div>
-    <b-container fluid v-bind:style="{ 'background-color': backgroundColor, 'color': foregroundColor}" id="image-details">
-      <b-row v-if="image && album" class="pt-3">
-        <b-col cols=12>
-          <b-breadcrumb>
-            <b-breadcrumb-item :text="album.name" :to="'/albums/' + album.id"/>
-            <b-breadcrumb-item :text="image.name" disabled active />
-          </b-breadcrumb>
-        </b-col>
-      </b-row>
-      <b-row v-if="image" >
-        <b-col cols=12 sm=12 md=12 lg=8 xl=9 class="img-col">
-          <a :href="baseUrl + 'image/' + image.id + '/img?small=false'">
-            <b-img fluid-grow :src="baseUrl + 'image/' + image.id + '/img?small=false'" class="img-details"/>
-          </a>
-        </b-col>
-        <b-col cols=12 sm=12 md=12 lg=4 xl=3 class="exif">
-          <div v-if="image.exif" class="mt-3">
-            <h3 v-if="image.exif && (image.exif.dateTimeOriginal || image.exif.dateTime)">Taken on: {{ getTime() | toDate }}</h3>
+    <transition>
+      <b-container fluid v-bind:style="{ 'background-color': backgroundColor, 'color': foregroundColor}" id="image-details">
+        <b-row v-if="image && album" class="pt-3">
+          <b-col cols=12>
+            <b-breadcrumb>
+              <b-breadcrumb-item :text="album.name" :to="'/albums/' + album.id"/>
+              <b-breadcrumb-item :text="image.name" disabled active />
+            </b-breadcrumb>
+          </b-col>
+        </b-row>
+        <b-row v-if="image" >
+          <b-col cols=12 sm=12 md=12 lg=8 xl=9 class="img-col">
+            <a :href="baseUrl + 'image/' + image.id + '/img?small=false'">
+              <b-img fluid-grow :src="baseUrl + 'image/' + image.id + '/img?small=false'" class="img-details"/>
+            </a>
+          </b-col>
+          <b-col cols=12 sm=12 md=12 lg=4 xl=3 class="exif">
+            <div v-if="image.exif" class="mt-3">
+              <h3 v-if="image.exif && (image.exif.dateTimeOriginal || image.exif.dateTime)">Taken on: {{ getTime() | toDate }}</h3>
+              <b-row>
+                <b-col cols=6>
+                  <img src="../assets/icon-camera.svg" fluid>
+                  <div>{{ image.exif.cameraMake }}</div><div>{{ image.exif.cameraModel }}</div>
+                </b-col>
+                <b-col cols=6>
+                  <div title="Aperture"><img src="../assets/icon-aperture.svg"> <span>{{ image.exif.fNumber ? image.exif.fNumber : '' }}</span></div>
+                  <div title="Exposure time"><img src="../assets/icon-exposure.svg"> <span>{{ image.exif.exposureTime ? image.exif.exposureTime : '' }}</span></div>
+                  <div title="Focal length"><img src="../assets/icon-focal-length.svg"> <span>{{ image.exif.focalLength ? image.exif.focalLength : '' }}</span></div>
+                  <div title="ISO"><img src="../assets/icon-iso.svg"> <span>{{ image.exif.isoSpeedRatings ? image.exif.isoSpeedRatings : '' }}</span></div>
+                  <div title="Flash" v-if="hasFlash()"><img src="../assets/icon-flash.svg"> <span>{{ image.exif.flash ? image.exif.flash : '' }}</span></div>
+                  <div title="Flash" v-else><img src="../assets/icon-flash-no.svg"> <span>{{ image.exif.flash ? image.exif.flash : '' }}</span></div>
+                </b-col>
+                <b-col cols=6 v-if="image.exif.lensMake || image.exif.lensModel">
+                  <img src="../assets/icon-lens.svg" fluid>
+                  <div>{{ image.exif.lensMake }}</div><div>{{ image.exif.lensModel }}</div>
+                </b-col>
+                <b-col cols=12>
+                  <hr class="white"/>
+                </b-col>
+                <template v-if="image.exif">
+                  <b-col cols=12>
+                    <a v-b-toggle.collapse-exif href="#" @click="$event.preventDefault()">Show full EXIF</a>
+                    <b-collapse id="collapse-exif" class="mt-2">
+                      <dl>
+                        <template v-for="(value, name) in image.exif">
+                          <dt :key="'dt-' + name">{{ name }}</dt>
+                          <dd :key="'dd-' + name">{{ value }}</dd>
+                        </template>
+                      </dl>
+                    </b-collapse>
+                  </b-col>
+                  <b-col cols=12>
+                    <hr class="white"/>
+                  </b-col>
+                </template>
+              </b-row>
+              <template v-if="image.exif && image.exif.gpsLatitude && image.exif.gpsLongitude">
+                <b-row>
+                  <b-col cols=12>
+                    <SingleLocationMap :latitude="image.exif.gpsLatitude" :longitude="image.exif.gpsLongitude" :zoom=5 />
+                  </b-col>
+                </b-row>
+                <b-row>
+                  <b-col cols=12>
+                    <hr class="white"/>
+                  </b-col>
+                </b-row>
+              </template>
+            </div>
             <b-row>
-              <b-col cols=6>
-                <img src="../assets/icon-camera.svg" fluid>
-                <div>{{ image.exif.cameraMake }}</div><div>{{ image.exif.cameraModel }}</div>
-              </b-col>
-              <b-col cols=6>
-                <div title="Aperture"><img src="../assets/icon-aperture.svg"> <span>{{ image.exif.fNumber ? image.exif.fNumber : '' }}</span></div>
-                <div title="Exposure time"><img src="../assets/icon-exposure.svg"> <span>{{ image.exif.exposureTime ? image.exif.exposureTime : '' }}</span></div>
-                <div title="Focal length"><img src="../assets/icon-focal-length.svg"> <span>{{ image.exif.focalLength ? image.exif.focalLength : '' }}</span></div>
-                <div title="ISO"><img src="../assets/icon-iso.svg"> <span>{{ image.exif.isoSpeedRatings ? image.exif.isoSpeedRatings : '' }}</span></div>
-                <div title="Flash" v-if="hasFlash()"><img src="../assets/icon-flash.svg"> <span>{{ image.exif.flash ? image.exif.flash : '' }}</span></div>
-                <div title="Flash" v-else><img src="../assets/icon-flash-no.svg"> <span>{{ image.exif.flash ? image.exif.flash : '' }}</span></div>
-              </b-col>
-              <b-col cols=6 v-if="image.exif.lensMake || image.exif.lensModel">
-                <img src="../assets/icon-lens.svg" fluid>
-                <div>{{ image.exif.lensMake }}</div><div>{{ image.exif.lensModel }}</div>
+              <b-col cols=12>
+                <div class="image-actions">
+                  <HeartIcon v-if="image.isFavorite" @click.native="onToggleFavorite($event)" title="Unmark as favorite"/>
+                  <HeartOutlineIcon v-else  @click.native="onToggleFavorite($event)" title="Mark as favorite"/>
+                  <FolderImageIcon title="Set image as album cover" @click.native="onSetImageAsAlbumCover($event)" />
+                </div>
               </b-col>
               <b-col cols=12>
-                <hr class="white"/>
+                <hr class="white" />
               </b-col>
-              <template v-if="image.exif">
-                <b-col cols=12>
-                  <a v-b-toggle.collapse-exif href="#" @click="$event.preventDefault()">Show full EXIF</a>
-                  <b-collapse id="collapse-exif" class="mt-2">
-                    <dl>
-                      <template v-for="(value, name) in image.exif">
-                        <dt :key="'dt-' + name">{{ name }}</dt>
-                        <dd :key="'dd-' + name">{{ value }}</dd>
-                      </template>
-                    </dl>
-                  </b-collapse>
-                </b-col>
-                <b-col cols=12>
-                  <hr class="white"/>
-                </b-col>
-              </template>
             </b-row>
-            <template v-if="image.exif && image.exif.gpsLatitude && image.exif.gpsLongitude">
-              <b-row>
-                <b-col cols=12>
-                  <SingleLocationMap :latitude="image.exif.gpsLatitude" :longitude="image.exif.gpsLongitude" :zoom=5 />
-                </b-col>
-              </b-row>
-              <b-row>
-                <b-col cols=12>
-                  <hr class="white"/>
-                </b-col>
-              </b-row>
-            </template>
-          </div>
-          <b-row>
-            <b-col cols=12>
-              <div class="image-actions">
-                <HeartIcon v-if="image.isFavorite" @click.native="onToggleFavorite($event)" title="Unmark as favorite"/>
-                <HeartOutlineIcon v-else  @click.native="onToggleFavorite($event)" title="Mark as favorite"/>
-                <FolderImageIcon title="Set image as album cover" @click.native="onSetImageAsAlbumCover($event)" />
-              </div>
-            </b-col>
-            <b-col cols=12>
-              <hr class="white" />
-            </b-col>
-          </b-row>
-          <div class="mt-3 tags">
-            <h3>Tags</h3>
-            <TagWidget :tags="tags"
-                       :id="image.id"
-                       :type="'image'"
-                       v-on:on-tag-deleted="updateTags"
-                       v-on:on-tag-added="updateTags" />
-          </div>
-        </b-col>
-      </b-row>
-    </b-container>
-    <SelectAlbumModal :image="image"
-                      ref="selectAlbumModal"
-                      v-on:onAlbumClicked="album => onAlbumClicked(album)" />
+            <div class="mt-3 tags">
+              <h3>Tags</h3>
+              <TagWidget :tags="tags"
+                        :id="image.id"
+                        :type="'image'"
+                        v-on:on-tag-deleted="updateTags"
+                        v-on:on-tag-added="updateTags" />
+            </div>
+          </b-col>
+        </b-row>
+      </b-container>
+      <SelectAlbumModal :image="image"
+                        ref="selectAlbumModal"
+                        v-on:onAlbumClicked="album => onAlbumClicked(album)" />
+    </transition>
   </div>
 </template>
 
@@ -259,5 +261,9 @@ export default {
   }
   .image-actions .material-design-icon.folder-image-icon:hover > .material-design-icon__svg {
       fill: #FFC312;
+  }
+
+  transition #image-details {
+    transition: background-color 4s;
   }
 </style>
