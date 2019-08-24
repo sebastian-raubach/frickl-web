55eaf25e6d70e5f1027ab9dfa92cefa26fdbde23
Sebastian Raubach <sebastian.raubach@hutton.ac.uk>
2019-08-24 19:45:43 +0100

    - FIX: Fixed an issue with the favourite image grid not respecting the image per page selection.

--- src/views/Favorites.vue
+++ src/views/Favorites.vue
@@ -2,7 +2,6 @@
   <b-container fluid v-if="images && images.length > 0" class="pt-3">
     <h1>Favorites</h1>
     <image-grid :imageCount="imageCount"
-                :imagesPerPage="imagesPerPage"
                 :images="images"
                 v-on:onImageNavigation="page => onImageNavigation(page)"/>
   </b-container>
@@ -10,17 +9,27 @@
 
 <script>
 import ImageGrid from '../components/ImageGrid.vue'
+import { mapGetters } from 'vuex'
 
 export default {
   data: function () {
     return {
       images: [],
-      imageCount: 0,
-      imagesPerPage: 12
+      imageCount: 0
     }
   },
   components: {
     'image-grid': ImageGrid
+  },
+  computed: {
+    ...mapGetters([
+      'imagesPerPage'
+    ])
+  },
+  watch: {
+    imagesPerPage: function (newValue, oldValue) {
+      this.onImageNavigation(1)
+    }
   },
   methods: {
     onImageNavigation: function (page) {
