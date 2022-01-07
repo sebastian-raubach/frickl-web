6b9abd0c7f93ddcc886119e1940ac98270f6b511
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2022-01-07 08:11:42 +0000

    - CHG: Moved automated builds to GitHub Actions.

--- docker/arm/setenv.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-totalMemKB=$(awk '/MemTotal:/ { print $2 }' /proc/meminfo)
-usagePercent=75
-let heapKB=$totalMemKB*$usagePercent/100
-let heapMB=$heapKB/1024
-let max=$(( $heapMB > 512 ? $heapMB : 512 ))
-export CATALINA_OPTS="$CATALINA_OPTS -Xmx${heapMB}m"
-export JAVA_OPTS="$JAVA_OPTS -Xmx${heapMB}m"
