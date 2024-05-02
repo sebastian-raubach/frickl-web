c95e504743c6cf8c59763b577aef83f5b4daf4be
sebastian-raubach <sebastian.raubach@hutton.ac.uk>
2024-05-02 18:16:10 +0100

    - FIX: Fixed missing marker icon on single location map.

--- src/components/SingleLocationMap.vue
+++ src/components/SingleLocationMap.vue
@@ -10,6 +10,14 @@ import { mapGetters } from 'vuex'
 import L from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
+// Set the leaflet marker icon
+delete L.Icon.Default.prototype._getIconUrl
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
+
 export default {
   props: {
     image: {
